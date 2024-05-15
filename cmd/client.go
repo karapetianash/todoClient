d621c6508d81f13a36efc40adc3057ec0f60019a
Ashot Karapetian <AshotArtashesi@yandex.ru>
2024-05-15 23:57:36 +0300

    add an option of viewing a single item

--- cmd/client.go
+++ cmd/client.go
@@ -9,6 +9,8 @@ import (
 	"time"
 )
 
+const timeFormat = "Jan/02 @15:04"
+
 var (
 	ErrConnection      = errors.New("connection error")
 	ErrNotFound        = errors.New("not found")
@@ -80,4 +82,20 @@ func getAll(apiRoot string) ([]item, error) {
 	u := fmt.Sprintf("%s/todo", apiRoot)
 
 	return getItems(u)
+}
+
+// getOne queries one item only
+func getOne(apiRoot string, id int) (item, error) {
+	u := fmt.Sprintf("%s/todo/%d", apiRoot, id)
+
+	items, err := getItems(u)
+	if err != nil {
+		return item{}, err
+	}
+
+	if len(items) != 1 {
+		return item{}, fmt.Errorf("%w: Invalid results", ErrInvalid)
+	}
+
+	return items[0], nil
 }
